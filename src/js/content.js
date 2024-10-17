8ef47660dad38ccf53acd05f2a4d4a921b9a9534
wendyhamel <whamel@it4web.nl>
2024-10-17 17:50:41 +0200

    Added tailwind to project changed html to reduce duplication bij adding code te js and show thi with alpine loop. use op picture tag

---
+++ src/js/content.js
@@ -0,0 +1,94 @@
+window.dataJS = function () {
+	return {
+		menuOpened: false,
+		menu: [
+			{ title: 'About', url: '#'},
+			{ title: 'Careers', url: '#'},
+			{ title: 'Events', url: '#'},
+			{ title: 'Products', url: '#'},
+			{ title: 'Support', url: '#'},
+		],
+		cards: [
+			{
+				id: 'card1',
+				images: {
+					mobile: 'images/mobile/image-deep-earth.jpg',
+					desktop: 'images/desktop/image-deep-earth.jpg',
+					alt: 'Deep earth'
+				},
+				title: 'Deep earth',
+				url: '#'
+			},
+			{
+				id: 'card2',
+				images: {
+					mobile: 'images/mobile/image-night-arcade.jpg',
+					desktop: 'images/desktop/image-night-arcade.jpg',
+					alt: 'Night arcade'
+				},
+				title: 'Night arcade',
+				url: '#'
+			},
+			{
+				id: 'card3',
+				images: {
+					mobile: 'images/mobile/image-soccer-team.jpg',
+					desktop: 'images/desktop/image-soccer-team.jpg',
+					alt: 'Soccer team VR'
+				},
+				title: 'Soccer team VR',
+				url: '#'
+			},
+			{
+				id: 'card4',
+				images: {
+					mobile: 'images/mobile/image-grid.jpg',
+					desktop: 'images/desktop/image-grid.jpg',
+					alt: 'The grid'
+				},
+				title: 'The grid',
+				url: '#'
+			},
+			{
+				id: 'card5',
+				images: {
+					mobile: 'images/mobile/image-from-above.jpg',
+					desktop: 'images/desktop/image-from-above.jpg',
+					alt: 'From up above VR'
+				},
+				title: 'From up above VR',
+				url: '#'
+			},
+			{
+				id: 'card6',
+				images: {
+					mobile: 'images/mobile/image-pocket-borealis.jpg',
+					desktop: 'images/desktop/image-pocket-borealis.jpg',
+					alt: 'Pocket borealis'
+				},
+				title: 'Pocket borealis',
+				url: '#'
+			},
+			{
+				id: 'card7',
+				images: {
+					mobile: 'images/mobile/image-curiosity.jpg',
+					desktop: 'images/desktop/image-curiosity.jpg',
+					alt: 'The curiosity'
+				},
+				title: 'The curiosity',
+				url: '#'
+			},
+			{
+				id: 'card8',
+				images: {
+					mobile: 'images/mobile/image-fisheye.jpg',
+					desktop: 'images/desktop/image-fisheye.jpg',
+					alt: 'Make it fisheye'
+				},
+				title: 'Make it fisheye',
+				url: '#'
+			},
+		]
+	}
+}
---
+++ src/js/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Alata', 'sans-serif'],
+        'display': ['Josefin Sans', 'sans-serif'],
+      },
+      fontSize: {
+        'body': ['15px', '22px'],
+      },
+      colors: {
+        gray: 'hsl(0, 0%, 55%)',
+        dark: 'hsl(0, 0%, 41%)',
+      }
+    },
+  },
+  plugins: [],
+}
+
