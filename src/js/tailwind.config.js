4a92bbfb67971ca6c4678a22fa5b8def50c8f61c
wendyhamel <whamel@it4web.nl>
2024-11-12 15:10:52 +0100

    fix tailwind.config.js and font

--- src/js/tailwind.config.js
+++ src/js/tailwind.config.js
@@ -1,11 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["index.html"],
+tailwind.config = {
+  content: ["index.html", "./src/content.js"],
   theme: {
     extend: {
       fontFamily: {
         'sans': ['Alata', 'sans-serif'],
-        'display': ['Josefin Sans', 'sans-serif'],
+        'display': ['Josefin\\ Sans', 'sans-serif'],
       },
       fontSize: {
         'body': ['15px', '22px'],
